507b0991799ce589c061bde88a07ced8ac96c2f5
AndersemReis <andersem.Reis@gmail.com>
2022-05-22 17:23:51 -0400

    Iniciando javascript

---
+++ javascript/scripts.js
@@ -0,0 +1,19 @@
+//console.log(`olha 'eu' 'no' "teste" "de" js`)\\
+/*console.log(12)
+console.log(24.5)
+console.log(12*24/4)
+console.log(Infinity)
+console.log(50+(53*4.5))
+console.log(Math.round(50+(53*4.5)))
+console.log(4>32)   */
+/*console.log({
+    marca: 'Fiat',
+    largura: '550cm',
+    peso: 1,
+    velocidade: (900*200) * 3,
+    andar: function () {
+        console.log(`sua velocidade é de 200`)
+    },
+    possuiar: true
+})*/
+
